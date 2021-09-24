d5747fdd213d44209755e1be0991d3260689c6e6
cristian2024 <cristiandramirezm.2024@gmail.com>
2021-09-24 09:28:37 -0500

    Creacion del principio del modal

--- src/views/InOut.vue
+++ src/views/InOut.vue
@@ -4,7 +4,7 @@
     <Navbar class="nav"></Navbar>
     <div class="main_entrada">
       <div class="superior-bar">
-        <img src="@/assets/add.svg" alt="">
+        <img src="@/assets/add.svg" @click="changeModal(true)" alt="">
         <SearchBar></SearchBar>
       </div>
       <div class="listado">
@@ -12,15 +12,22 @@
       </div>
     </div>
   </div>
+
+  <div class="modal" v-if="showModalNewEntrada" >
+      <ModalNew></ModalNew>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+// import { mapState, mapMutations } from 'vuex';
+
 import Header from '@/components/Header.vue';
 import Navbar from '@/components/Nav.vue';
 import SearchBar from '@/components/SearchButton.vue';
 import EntradaSalida from '@/components/EntradaSalida.vue';
+import ModalNew from '@/components/ModalNewEntrada.vue';
 
 export default {
   name: 'InOut',
@@ -29,11 +36,29 @@ export default {
     Navbar,
     SearchBar,
     EntradaSalida,
+    ModalNew,
+  },
+  data() {
+  },
+  computed: {
+    // ...mapState('showModalNewEntrada'),
+    showModal() {
+      return this.$store.getters.showModalNewEntrada;
+    },
+  },
+  methods: {
+    // ...mapMutations('changeShowModalNewEntrada'),
+    changeModal(value) {
+      // alert(value);
+      this.$store.dispatch('changeModalNewEntrada', value);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+
+@import '@/views/scss/_theme.scss';
   .listado{
     margin-top: 20px;
   }
@@ -53,6 +78,17 @@ export default {
       display: block;
 
     }
+  }
+
+  .modal{
+    position: fixed;
+    display: flex; /* establish flex container */
+    justify-content: center; /* center flex items horizontally, in this case */
+    align-items: center; /* center flex items vertically, in this case */
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100%;
+    width: 100%;
+    top: 0;
   }
 
 </style>
