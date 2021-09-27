0296fd96e5f847919e03fe01541882245b5cd1bf
ramtako8922 <67653677+ramtako8922@users.noreply.github.com>
2021-09-26 23:19:21 -0500

    Add files via upload

---
+++ src/views/Residentes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Header/>
+    <Nav/>
+    <ListaResidentes />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListaResidentes from '@/components/ListaResidentes.vue';
+import Header from '@/components/Header.vue';
+import Nav from '@/components/Nav.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ListaResidentes,
+    Header,
+    Nav,
+  },
+};
+</script>
---
+++ src/views/Select_tower.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <Header></Header>
+    <Navbar></Navbar>
+    <button class="btn-open" @click="showTorre()">probar pop-ups</button>
+    <div>
+    <STower v-if="showModal" @close="showModal=false"/>
+    </div>
+     
+    </div>
+    
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '@/components/Header.vue';
+import Navbar from '@/components/Nav.vue';
+import STower from '@/components/ModalSelecTower.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Navbar,
+    STower
+    
+  },
+  data(){
+    return{
+      showModal: false
+  }
+  },
+  methods:{
+    showTorre(){
+    this.showModal=true
+
+  }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
