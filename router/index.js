2ac6de7bc2088d8557e7ea794971a09757eb3e61
ramtako8922 <67653677+ramtako8922@users.noreply.github.com>
2021-09-26 20:47:00 -0500

    agregando rutas

---
+++ router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+import InOut from '../views/InOut.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: InOut,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/vehicle_zone',
+    name: 'Vehicle_zone',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/vehicle_zone.vue'),
+  },
+  {
+    path: '/residentes',
+    name: 'Residentes',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Residentes.vue'),
+  },
+
+  {
+    path: '/select_tower',
+    name: 'Select_tower',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Select_tower.vue'),
+  },
+
+  {
+    path: '/infoapto',
+    name: 'Info_apto',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Info_apto.vue'),
+  },
+
+  {
+    path: '/infovehiculoparqueado',
+    name: 'Info_vehiculo_parqueadero',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Info_vehiculo_parqueadero.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
